13df6d0c8d18098d2b1779d06830b7d995c2cd49
xiaojue <designsor@gmail.com>
2014-01-10 16:25:05 +0800

    add room.js and socket.io

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var port = 5566;
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 app.use(express['static']('public'));
 app.engine('.html', require('ejs').__express);
@@ -19,14 +21,31 @@ app.get('/', function(req, res, next) {
 });
 
 app.get('/rank', function(req, res, next) {
-    res.render('rank');
+	res.render('rank');
 });
 
-app.get('/handle',function(req,res,next){
-    res.render('handle');
+app.get('/handle', function(req, res, next) {
+	res.render('handle');
 });
 
-app.listen(port);
+io.sockets.on('connection', function(socket) {
+	socket.on('add', function(data) {
+		console.log(data);
+	});
+	socket.on('start', function(data) {
+
+	});
+	socket.on('stop', function(data) {
+
+	});
+	socket.on('top', function() {
+
+	});
+	socket.on('right', function() {});
+	socket.on('down', function() {});
+	socket.on('left', function() {});
+});
+
+server.listen(port);
 
 console.log('view 127.0.0.1:' + port);
-
