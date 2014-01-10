fdbe88680c2b76e882f8bd5e397f87b87d9de7fb
xiaojue <designsor@gmail.com>
2014-01-10 16:58:43 +0800

    add room.js

--- public/room.js
+++ public/room.js
@@ -1,4 +1,11 @@
-(function(global,undef){
-    var socket = io.connect('http://172.16.121.205:5566');
-    socket.emit('add',{user:'xiaojue'});
+(function(global, undef) {
+	$(function() {
+		var socket = io.connect('http://172.16.121.205:5566');
+		//初始化房间信息
+		socket.on('status', function(data) {
+			alert(data.length);
+		});
+		socket.emit('adduser');
+	});
 })(this);
+
